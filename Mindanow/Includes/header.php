cc46821c19d4135e789597b220b24b72ac76693a
CCimagala <c.cimagala.482520@umindanao.edu.ph>
2026-03-11 17:53:58 +0800

    Delete Mindanow directory

--- Mindanow/Includes/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mindanow</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
